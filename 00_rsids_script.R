3a550bab37d09097951d10ce668b91d8d20cd697
Mpatel41 <94924921+Mpatel41@users.noreply.github.com>
2023-11-28 13:08:29 -0600

    Adding RSIDS to the sumstats

---
+++ 00_rsids_script.R
@@ -0,0 +1,10 @@
+#Adding the RSIDS to the sum stats 
+
+#!/bin/bash
+python3 /home/wheelerlab3/Data/dbSNP_annotations/cpos2rsid.py \
+-i /home/mansi/continuous-50-both_sexes-irnt.tsv.bgz \
+-o rsids_height_gwas \
+-c chr \
+-ea alt \
+-oa ref \
+-p pos
